5e3aca594730b1e0f906335fa523330d8905f084
Hamhire <me@huhamhire.com>
2014-10-09 20:51:05 +0800

    Add GroupByTestCase and OrderByTestCase

--- test/dml_test.py
+++ test/dml_test.py
@@ -14,9 +14,10 @@
 
 __author__ = "huhamhire <me@huhamhire.com>"
 
+import re
 import unittest
 
-from data.dml import Where, Having
+from data.dml import Where, Having, GroupBy, OrderBy
 from data.dialect import Dialect
 from data.constants import (
     ValueTypes, CompareTypes, RelationTypes, AggregateFunctions)
@@ -103,6 +104,11 @@ class HavingTestCase(unittest.TestCase):
             "compare_type": CompareTypes.LESS_THAN_OR_EQUAL,
             "relation_type": RelationTypes.OR
         }
+        basic_int_no_func = {
+            "column_name": "alpha",
+            "column_value": 1,
+            "column_type": ValueTypes.INTEGER
+        }
 
     def setUp(self):
         self.having = Having()
@@ -124,8 +130,79 @@ class HavingTestCase(unittest.TestCase):
         result = self.having.to_sql(self.dialect)
         self.assertEqual(result, expected)
 
-if __name__ == "__main__":
+    def test_one_column_no_func(self):
+        flag = 0
+        try:
+            self.having.add_column(**self.TestColumns.basic_int_no_func)
+        except TypeError, e:
+            expected = "TypeError"
+            self.assertRaisesRegexp(e, re.compile(expected))
+            flag += 1
+        self.assertEqual(flag, 1)
+
+
+class GroupByTestCase(unittest.TestCase):
+    class TestColumns(object):
+        column_alpha = {"column_name": "alpha"}
+        column_beta = {"column_name": "beta"}
+
+    def setUp(self):
+        self.group_by = GroupBy()
+        self.dialect = Dialect()
+
+    def tearDown(self):
+        self.group_by.clear()
+
+    def test_one_column(self):
+        self.group_by.add_column(**self.TestColumns.column_alpha)
+        expected = " GROUP BY alpha"
+        result = self.group_by.to_sql(self.dialect)
+        self.assertEqual(result, expected)
+
+    def test_two_columns(self):
+        self.group_by.add_column(**self.TestColumns.column_alpha)
+        self.group_by.add_column(**self.TestColumns.column_beta)
+        expected = " GROUP BY alpha, beta"
+        result = self.group_by.to_sql(self.dialect)
+        self.assertEqual(result, expected)
+
+
+class OrderByTestCase(unittest.TestCase):
+    class TestColumns(object):
+        column_alpha_asc = {"column_name": "alpha"}
+        column_beta_desc = {"column_name": "beta", "asc": False}
+
+    def setUp(self):
+        self.order_by = OrderBy()
+        self.dialect = Dialect()
+
+    def tearDown(self):
+        self.order_by.clear()
+
+    def test_one_column_asc(self):
+        self.order_by.add_column(**self.TestColumns.column_alpha_asc)
+        expected = " ORDER BY alpha"
+        result = self.order_by.to_sql(self.dialect)
+        self.assertEqual(result, expected)
+
+    def test_two_columns_mix(self):
+        self.order_by.add_column(**self.TestColumns.column_alpha_asc)
+        self.order_by.add_column(**self.TestColumns.column_beta_desc)
+        expected = " ORDER BY alpha, beta DESC"
+        result = self.order_by.to_sql(self.dialect)
+        self.assertEqual(result, expected)
+
+
+def get_dml_test_suite():
     where_test_suite = unittest.makeSuite(WhereTestCase, "test")
     having_test_suite = unittest.makeSuite(HavingTestCase, "test")
-    dml_test = unittest.TestSuite((where_test_suite, having_test_suite))
-    unittest.TextTestRunner(verbosity=1).run(dml_test)
+    group_by_test_suite = unittest.makeSuite(GroupByTestCase, "test")
+    order_by_test_suite = unittest.makeSuite(OrderByTestCase, "test")
+    dml_test = unittest.TestSuite((
+        where_test_suite, having_test_suite, group_by_test_suite,
+        order_by_test_suite
+    ))
+    return dml_test
+
+if __name__ == "__main__":
+    unittest.TextTestRunner(verbosity=1).run(get_dml_test_suite())
